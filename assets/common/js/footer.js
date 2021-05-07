cf202cff75ca58316de1e48de280fe4ca4d4dee3
Rooney <roneysantos7711@gmail.com>
2021-05-07 18:10:47 -0300

    Teste caminho 2

--- assets/common/js/footer.js
+++ assets/common/js/footer.js
@@ -4,6 +4,11 @@
 });*/
 
 //Rodape
-$(document).ready(function(){
+/*$(document).ready(function(){
     $("#footer").load("/views/footer.html");
+});*/
+
+//Quando for subir
+$(document).ready(function(){
+    $("#footer").load("/proj-extensao/views/footer.html");
 });
