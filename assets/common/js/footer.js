b5d858487a5071c4bbe652635f810bc4e0bf7a2b
Gustavo Navarro <gustavo_navarro2000@hotmail.com>
2021-05-13 00:58:30 -0300

    um monte de coisa

--- assets/common/js/footer.js
+++ assets/common/js/footer.js
@@ -4,11 +4,12 @@
 });*/
 
 //Rodape
-/*$(document).ready(function(){
+//Local
+$(document).ready(function(){
     $("#footer").load("/views/footer.html");
-});*/
+});
 
 //Quando for subir
-$(document).ready(function(){
-    $("#footer").load("/proj-extensao/views/footer.html");
-});
+// $(document).ready(function(){
+//     $("#footer").load("/proj-extensao/views/footer.html");
+// });
