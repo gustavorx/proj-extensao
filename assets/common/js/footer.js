1a664eb05f1cd9dd8dbd0bc762a20c3c0db6f733
Gustavo Navarro <gustavo_navarro2000@hotmail.com>
2021-05-19 21:50:04 -0300

    alterando rota do footer no JS

--- assets/common/js/footer.js
+++ assets/common/js/footer.js
@@ -1,18 +1,11 @@
-//Menu | Função de hover não funciona importando
-/*$(document).ready(function(){
-    $("#header").load("/views/header.html");
-});*/
-
 //Rodape
+
 //Local
 $(document).ready(function(){
-    $("#footer").load("/views/footer.html");
+    $("#footer").load("../views/footer.html");
 });
 
 // Quando for subir
 // $(document).ready(function(){
 //     $("#footer").load("/proj-extensao/views/footer.html");
 // });
-
-
-//COMENTARIO PARA IGNORAR
