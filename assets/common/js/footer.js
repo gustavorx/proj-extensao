ee758ddf5a09d0dc18a557980c5d57b8261d79a1
Gustavo Navarro <gustavo_navarro2000@hotmail.com>
2021-05-19 21:51:02 -0300

    limpando footer.js

--- assets/common/js/footer.js
+++ assets/common/js/footer.js
@@ -1,11 +1,3 @@
-//Rodape
-
-//Local
 $(document).ready(function(){
     $("#footer").load("../views/footer.html");
 });
-
-// Quando for subir
-// $(document).ready(function(){
-//     $("#footer").load("/proj-extensao/views/footer.html");
-// });
