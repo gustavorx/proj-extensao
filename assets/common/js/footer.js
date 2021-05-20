9ccc4ef89b29ad1923ed27a9c21cccd3ec64a5a8
Gustavo Navarro <gustavo_navarro2000@hotmail.com>
2021-05-19 21:55:22 -0300

    voltando o endereco do footer.js

--- assets/common/js/footer.js
+++ assets/common/js/footer.js
@@ -1,3 +1,11 @@
+//Rodape
+
+//Local
+// $(document).ready(function(){
+//     $("#footer").load("../views/footer.html");
+// });
+
+// Quando for subir
 $(document).ready(function(){
-    $("#footer").load("../views/footer.html");
+    $("#footer").load("/proj-extensao/views/footer.html");
 });
